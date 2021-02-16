d9837b9f340cdeb1ac25365bfecdaf7140583388
JayChou <473811301@qq.com>
2021-02-16 16:00:37 +0800

    添加了测试用的定时任务代码

--- src/main/java/com/zhaoyang/demo/DemoApplication.java
+++ src/main/java/com/zhaoyang/demo/DemoApplication.java
@@ -3,7 +3,9 @@ package com.zhaoyang.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @MapperScan("com.zhaoyang.demo.dao.mapper")
 @SpringBootApplication
 public class DemoApplication {
